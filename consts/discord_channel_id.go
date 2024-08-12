7af0003db724799a2236efb4613afa67a3720531
mizukichi0210 <canaryyellow.nl@gmail.com>
2024-08-12 20:59:51 +0900

    discord channeの追加(途中)

---
+++ consts/discord_channel_id.go
@@ -0,0 +1,51 @@
+package consts
+
+// hololive全体
+const (
+	// hololive全体
+	HololiveJPAll = "1253710858240921740"
+	// EN
+	HololiveENAll = "1259585771833135144"
+
+	// ID
+	HololiveIDAll = "1259585707219877931"
+
+	// DEV_IS
+	HololiveDEVISAll = "1259586334935482468"
+)
+
+// 0期生
+const (
+	// ときのそら
+	TokinoSora = "1253704712931246110"
+
+	// ロボ子さん
+	RobocoSan = "1253704752173285406"
+
+	// さくらみこ
+	SakuraMiko = "1253704783789817979"
+
+	// 星街すいせい
+	HoshimachiSuisei = "1253704813846462475"
+
+	// AZKi
+	Azki = "1253704874856812635"
+)
+
+// 1期生
+const (
+	// 夜空メル
+	YozoraMel = "1253705014157770843"
+
+	// アキ・ローゼンタール
+	AkiRozenthal = "1253705047636709386"
+
+	// 赤井はあと
+	AkaiHaato = "1253705093790961805"
+
+	// 白上フブキ
+	ShirakamiFubuki = "1253705132697325679"
+
+	// 夏色まつり
+	NatsuiroMatsuri = "1253705170949243007"
+)
