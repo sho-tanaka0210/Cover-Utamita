c422f59cb235ecaf7b0c65b728236b32132a4ba5
mizukichi0210 <canaryyellow.nl@gmail.com>
2024-09-01 12:57:06 +0000

    定数の修正

--- consts/url.go
+++ consts/url.go
@@ -3,7 +3,8 @@ package consts
 // GET
 const (
 	SearchVideo string = "https://www.googleapis.com/youtube/v3/videos"
-	Query       string = "歌ってみた | Cover | cover | Original Song"
+	Query       string = `歌ってみた Cover cover "Original Song"`
+	VideoUrl    string = "https://www.youtube.com/watch?v="
 )
 
 // POST
