92b735c44025a10c5e306ffd7611d92a3e21a9d4
mizukichi0210 <canaryyellow.nl@gmail.com>
2024-09-01 12:58:16 +0000

    メッセージ投稿の実装を修正

--- infrastructure/post_message.go
+++ infrastructure/post_message.go
@@ -1,17 +1,23 @@
 package infrastructure
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"cover-utamita/consts"
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 type Channel struct {
 	DiscordId string
-	ChannelId string
 	Url       string
 }
 
 func (c Channel) SendMessage(d *discordgo.Session) error {
 
-	_, err := d.ChannelMessageSend(c.DiscordId, c.Url)
+	videoUrl := fmt.Sprintf("%s%s", consts.VideoUrl, c.Url)
+	_, err := d.ChannelMessageSend(c.DiscordId, videoUrl)
 	if err != nil {
+		fmt.Printf("Discordへの投稿に失敗しました。 : %v", err)
 		return err
 	}
 
