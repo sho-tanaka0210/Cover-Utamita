0e17131dfc066cf8e2e8b4ac7d905d659f299d3a
mizukichi0210 <canaryyellow.nl@gmail.com>
2024-09-04 12:35:31 +0000

    YoutubeAPI周りのことを行うドメインの作成

---
+++ domain/youtube_search.go
@@ -0,0 +1,61 @@
+package domain
+
+import (
+	"cover-utamita/consts"
+	"fmt"
+	"strings"
+
+	"google.golang.org/api/youtube/v3"
+)
+
+// YouTubeAPIを用いて動画検索を行う
+//
+//	service:        YouTube Serviceクライアント
+//	channelId:      指定のチャンネルID
+//	publishedAfter: どれくらい前の日付から検索するか
+//	maxResults:     検索結果の最大数
+//
+//	results:        検索結果
+//	err:            エラー
+func SearchVideoes(service *youtube.Service, channelId string, publishedAfter string, maxResults int64) (results *youtube.SearchListResponse, err error) {
+
+	call := service.Search.List([]string{"id", "snippet"}).Q(consts.Query).ChannelId(channelId).PublishedAfter(publishedAfter).MaxResults(maxResults)
+	results, err = call.Do()
+	if err != nil {
+		fmt.Printf("APIリクエストに失敗しました: %v", err)
+		return nil, err
+	}
+
+	return results, nil
+}
+
+// ChannelIdやPublishedAfterからとってきた動画から、動画タイトルによる動画抽出を行う。
+//
+//	items: 検索結果一覧
+//	member: 検索をしているメンバー情報
+//
+//	results: 抽出結果
+func VideoRetrieval(items []*youtube.SearchResult, member consts.Constant) (results []Result) {
+	for _, item := range items {
+		if item.Id.Kind == "youtube#video" {
+			title := item.Snippet.Title
+			if titleRetrieval(title) {
+
+				results = append(results, Result{ChannelId: item.Id.ChannelId, Url: item.Id.VideoId, DiscordId: member.DiscordId()})
+			}
+		}
+	}
+
+	return results
+}
+
+func titleRetrieval(title string) bool {
+
+	return strings.Contains(title, consts.Utattemita) ||
+		strings.Contains(title, consts.CoverLarge) ||
+		strings.Contains(title, consts.CoverSmall) ||
+		strings.Contains(title, consts.OriginalSong) ||
+		strings.Contains(title, consts.Original) ||
+		strings.Contains(title, consts.CoveredByLarge) ||
+		strings.Contains(title, consts.CoveredBySmall)
+}
