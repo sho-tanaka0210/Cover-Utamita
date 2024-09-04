ebf054c953ae67f3eba72b627647f65b4ad3ce11
mizukichi0210 <canaryyellow.nl@gmail.com>
2024-09-04 12:36:14 +0000

    JSONファイルを作成するように実装修正

--- app.go
+++ app.go
@@ -3,22 +3,47 @@ package main
 import (
 	domain "cover-utamita/domain/hololive"
 	"cover-utamita/infrastructure"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func App(d *discordgo.Session) error {
 	// hololive
+	today := time.Now().Format("20060102")
+	filename := today + ".json"
+	if _, err := os.Stat(filename); err == nil {
+	} else if os.IsNotExist(err) {
+	} else {
+		fmt.Println("ファイルの状態確認に失敗しました:", err)
+		return err
+	}
+
 	r, err := domain.SearchVideoes()
 	if err != nil {
+		fmt.Printf("YouTubeAPIによる取得に失敗しました。 : %v", err)
+		return err
+	}
+
+	jsonData, err := json.MarshalIndent(r, "", "  ")
+	if err != nil {
+		fmt.Println("JSON変換に失敗しました:", err)
+		return err
+	}
+	err = os.WriteFile(filename, jsonData, 0644)
+	if err != nil {
+		fmt.Println("ファイルへの書き込みに失敗しました:", err)
 		return err
 	}
 
 	for _, v := range r {
-		c := infrastructure.Channel{ChannelId: v.ChannelId, Url: v.Url, DiscordId: v.DiscordId}
+		c := infrastructure.Channel{Url: v.Url, DiscordId: v.DiscordId}
 		err = c.SendMessage(d)
 		if err != nil {
-			return err
+			continue
 		}
 	}
 
