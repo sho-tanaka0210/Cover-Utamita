9734f80ba7d7b0db68e6f3677bf08f4089106b33
mizukichi0210 <canaryyellow.nl@gmail.com>
2024-11-10 00:13:06 +0900

    修正

--- domain/hololive/search_video.go
+++ domain/hololive/search_video.go
@@ -17,30 +17,30 @@ func SearchVideoes() (results []domain.Result, err error) {
 		// ALL
 		infrastructure.HololiveAll{Members: consts.Whole},
 
-		// // JP
-		// infrastructure.Gen0{Members: consts.Gen0s},
-		// infrastructure.Gen1{Members: consts.Gen1s},
-		// infrastructure.Gamers{Members: consts.Gamerses},
-		// infrastructure.Gen2{Members: consts.Gen2s},
-		// infrastructure.Gen3{Members: consts.Gen3s},
-		// infrastructure.Gen4{Members: consts.Gen4s},
-		// infrastructure.Gen5{Members: consts.Gen5s},
+		// JP
+		infrastructure.Gen0{Members: consts.Gen0s},
+		infrastructure.Gen1{Members: consts.Gen1s},
+		infrastructure.Gamers{Members: consts.Gamerses},
+		infrastructure.Gen2{Members: consts.Gen2s},
+		infrastructure.Gen3{Members: consts.Gen3s},
+		infrastructure.Gen4{Members: consts.Gen4s},
+		infrastructure.Gen5{Members: consts.Gen5s},
 
-		// infrastructure.HoloX{Members: consts.Holoxes},
+		infrastructure.HoloX{Members: consts.Holoxes},
 
-		// infrastructure.ReGross{Members: consts.ReGrosses},
-		// infrastructure.FlowGlow{Members: consts.FlowGlows},
+		infrastructure.ReGross{Members: consts.ReGrosses},
+		infrastructure.FlowGlow{Members: consts.FlowGlows},
 
-		// // ID
-		// infrastructure.IdGen1{Members: consts.IdGen1s},
-		// infrastructure.IdGen2{Members: consts.IdGen2s},
-		// infrastructure.IdGen3{Members: consts.IdGen3s},
+		// ID
+		infrastructure.IdGen1{Members: consts.IdGen1s},
+		infrastructure.IdGen2{Members: consts.IdGen2s},
+		infrastructure.IdGen3{Members: consts.IdGen3s},
 
-		// // EN
-		// infrastructure.Myth{Members: consts.Mythes},
-		// infrastructure.Promise{Members: consts.Promises},
-		// infrastructure.Justice{Members: consts.Jutsices},
-		// infrastructure.Advent{Members: consts.Advents},
+		// EN
+		infrastructure.Myth{Members: consts.Mythes},
+		infrastructure.Promise{Members: consts.Promises},
+		infrastructure.Justice{Members: consts.Jutsices},
+		infrastructure.Advent{Members: consts.Advents},
 	}
 
 	for _, members := range hololive {
